8617d39b6295fd4ddfc7699aacbce582e8827930
FTC13460 <164567446+heidi3brey@users.noreply.github.com>
2026-09-25 15:21:25 -0500

    variables, gamepad, tutorials
    variables, gamepad, tutorials

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GamePadPractice.java
@@ -0,0 +1,32 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@Disabled
+@TeleOp
+public class GamePadPractice extends OpMode {
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+        // runs 50x a second
+        double speedForward = -gamepad1.left_stick_y / 2.0;
+        double difference = gamepad1.left_stick_x-gamepad1.right_stick_x;
+        double sumTriggers = gamepad1.left_trigger + gamepad1.right_trigger;
+
+        telemetry.addData("xleft",gamepad1.left_stick_x);
+        telemetry.addData("yleft",gamepad1.left_stick_y);
+        telemetry.addData("a button",gamepad1.a);
+        telemetry.addData("speedForward",speedForward);
+        telemetry.addData("xright",gamepad1.right_stick_x);
+        telemetry.addData("yright",gamepad1.right_stick_y);
+        telemetry.addData("b button",gamepad1.b);
+        telemetry.addData("xright and xleft", difference);
+        telemetry.addData("sum triggers", sumTriggers);
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VariableExamples.java
@@ -0,0 +1,17 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+
+@Disabled
+public class VariableExamples {
+    // data types
+    int robotHeight; // rounded number
+    double motorSpeed; // floating number 0.445476
+    boolean clawClosed; // closed or open
+
+    //robotHeight = 10;
+    //motorSpeed = 0.5;
+    //clawClosed = true;
+
+
+}
